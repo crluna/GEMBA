da7c9cd19457426c9e4fa608381b372e720e6b22
carlos <carlos.luna@crugroup.com>
2024-08-12 14:11:25 +0100

    no hooks, remove bare exeptions

--- gemba/gemba_mqm_utils.py
+++ gemba/gemba_mqm_utils.py
@@ -1,7 +1,7 @@
-import ipdb
 import json
 import re
 from collections import defaultdict
+
 
 def apply_template(template, data):
     if isinstance(template, str):
@@ -10,11 +10,12 @@ def apply_template(template, data):
         prompt = []
         for conversation_turn in template:
             p = conversation_turn.copy()
-            p['content'] = p['content'].format(**data)
+            p["content"] = p["content"].format(**data)
             prompt.append(p)
         return prompt
     else:
         raise ValueError(f"Unknown template type {type(template)}")
+
 
 def parse_broken_json(x):
     improved_translation = ""
@@ -30,10 +31,11 @@ def parse_broken_json(x):
 
         try:
             errors = json.loads(data[1])
-        except:
+        except Exception as e:
+            print(e)
             # just try to get error count
-            words = re.findall(r'\b\w+\b', data[1].lower())
-            keywords = ['critical', 'major', 'minor']
+            words = re.findall(r"\b\w+\b", data[1].lower())
+            keywords = ["critical", "major", "minor"]
 
             last_key = None
             for word in words:
@@ -56,7 +58,14 @@ def parse_error_class(error):
                 class_name = f"accuracy-{subclass}"
     elif "fluency" in error:
         class_name = "fluency"
-        for subclass in ["character encoding", "grammar", "inconsistency", "punctuation", "register", "spelling"]:
+        for subclass in [
+            "character encoding",
+            "grammar",
+            "inconsistency",
+            "punctuation",
+            "register",
+            "spelling",
+        ]:
             if subclass in error:
                 class_name = f"fluency-{subclass}"
     elif "locale convention" in error:
@@ -79,7 +88,7 @@ def parse_error_class(error):
     return class_name
 
 
-def parse_mqm_answer(x, list_mqm_errors=False, full_desc=True):
+def parse_mqm_answer(x, list_mqm_errors=False, full_desc=True, add_scores=False):
     if x is None:
         return None
 
@@ -87,16 +96,16 @@ def parse_mqm_answer(x, list_mqm_errors=False, full_desc=True):
     if x.startswith('{"improved translation"'):
         try:
             x = json.loads(x)
-        except:
+        except Exception as e:
+            print(e)
             x = parse_broken_json(x)
         errors = x["errors"]
 
-
     else:
         x = x.lower()
-        errors = {'critical': [], 'major': [], 'minor': []}
+        errors = {"critical": [], "major": [], "minor": []}
         error_level = None
-        for line in x.split('\n'):
+        for line in x.split("\n"):
             line = line.strip()
             if "no-error" in line or "no error" in line or "" == line:
                 continue
@@ -111,7 +120,20 @@ def parse_mqm_answer(x, list_mqm_errors=False, full_desc=True):
                 continue
 
             if "critical" in line or "major" in line or "minor" in line:
-                if not any([line.startswith(x) for x in ['accuracy', 'fluency', 'locale convention', 'style', 'terminology', 'non-translation', 'other']]):
+                if not any(
+                    [
+                        line.startswith(x)
+                        for x in [
+                            "accuracy",
+                            "fluency",
+                            "locale convention",
+                            "style",
+                            "terminology",
+                            "non-translation",
+                            "other",
+                        ]
+                    ]
+                ):
                     print(line)
 
             if error_level is None:
@@ -126,12 +148,19 @@ def parse_mqm_answer(x, list_mqm_errors=False, full_desc=True):
     error_classes = defaultdict(list)
     final_score = 0
     error_counter = 0
-    for error_level in ['critical', 'major', 'minor']:
+    for error_level in ["critical", "major", "minor"]:
         if error_level not in errors:
-                continue
+            continue
         for error in errors[error_level]:
-            if error_counter < 5 and not list_mqm_errors:
-                final_score += 25 if error_level == 'critical' else 5 if error_level == 'major' else 1
+            # if error_counter < 5 and not list_mqm_errors:
+            if error_counter < 5:
+                final_score += (
+                    25
+                    if error_level == "critical"
+                    else 5
+                    if error_level == "major"
+                    else 1
+                )
                 error_counter += 1
 
             if full_desc:
@@ -143,7 +172,11 @@ def parse_mqm_answer(x, list_mqm_errors=False, full_desc=True):
         final_score = 25
 
     if list_mqm_errors:
-        return error_classes
+        if add_scores:
+            return error_classes, -final_score
+        else:
+            return error_classes
+
     else:
         # negative score is to normalize that higher score is better
         return -final_score
@@ -153,7 +186,7 @@ def mqm_fewshot(few_shots):
     prompts = [
         {
             "role": "system",
-            "content": f"You are an annotator for the quality of machine translation. Your task is to identify errors and assess the quality of the translation."
+            "content": "You are an annotator for the quality of machine translation. Your task is to identify errors and assess the quality of the translation.",
         }
     ]
 
@@ -163,34 +196,25 @@ def mqm_fewshot(few_shots):
 ```{target_seg}```
 
 Based on the source segment and machine translation surrounded with triple backticks, identify error types in the translation and classify them. The categories of errors are: accuracy (addition, mistranslation, omission, untranslated text), fluency (character encoding, grammar, inconsistency, punctuation, register, spelling), style (awkward), terminology (inappropriate for context, inconsistent use), non-translation, other, or no-error.\nEach error is classified as one of three categories: critical, major, and minor. Critical errors inhibit comprehension of the text. Major errors disrupt the flow, but what the text is trying to say is still understandable. Minor errors are technically errors, but do not disrupt the flow or hinder comprehension."""
-   
+
     for shot in few_shots:
-        prompts.append({
-            "role": "user",
-            "content": template.format(**shot)
-        })
-        answer = shot['answer']
+        prompts.append({"role": "user", "content": template.format(**shot)})
+        answer = shot["answer"]
 
-        prompts.append({
-            "role": "assistant",
-            "content": answer
-        })
+        prompts.append({"role": "assistant", "content": answer})
 
-    prompts.append({
-            "role": "user",
-            "content": template
-        })
+    prompts.append({"role": "user", "content": template})
 
     return prompts
 
 
 few_shots = {
     "ende": {
-            "source_lang": "English",
-            "source_seg": "I do apologise about this, we must gain permission from the account holder to discuss an order with another person, I apologise if this was done previously, however, I would not be able to discuss this with yourself without the account holders permission.",
-            "target_lang": "German",
-            "target_seg": "Ich entschuldige mich dafür, wir müssen die Erlaubnis einholen, um eine Bestellung mit einer anderen Person zu besprechen. Ich entschuldige mich, falls dies zuvor geschehen wäre, aber ohne die Erlaubnis des Kontoinhabers wäre ich nicht in der Lage, dies mit dir involvement.",
-            "answer": """Critical:
+        "source_lang": "English",
+        "source_seg": "I do apologise about this, we must gain permission from the account holder to discuss an order with another person, I apologise if this was done previously, however, I would not be able to discuss this with yourself without the account holders permission.",
+        "target_lang": "German",
+        "target_seg": "Ich entschuldige mich dafür, wir müssen die Erlaubnis einholen, um eine Bestellung mit einer anderen Person zu besprechen. Ich entschuldige mich, falls dies zuvor geschehen wäre, aber ohne die Erlaubnis des Kontoinhabers wäre ich nicht in der Lage, dies mit dir involvement.",
+        "answer": """Critical:
 no-error
 Major:
 accuracy/mistranslation - "involvement"
@@ -199,13 +223,13 @@ Minor:
 fluency/grammar - "wäre"
 fluency/register - "dir"
 """,
-        },
+    },
     "encs": {
-            "source_lang": "English",
-            "source_seg": "Talks have resumed in Vienna to try to revive the nuclear pact, with both sides trying to gauge the prospects of success after the latest exchanges in the stop-start negotiations.",
-            "target_lang": "Czech",
-            "target_seg": "Ve Vídni se ve Vídni obnovily rozhovory o oživení jaderného paktu, přičemž obě partaje se snaží posoudit vyhlídky na úspěch po posledních výměnách v jednáních.",
-            "answer": """Critical:
+        "source_lang": "English",
+        "source_seg": "Talks have resumed in Vienna to try to revive the nuclear pact, with both sides trying to gauge the prospects of success after the latest exchanges in the stop-start negotiations.",
+        "target_lang": "Czech",
+        "target_seg": "Ve Vídni se ve Vídni obnovily rozhovory o oživení jaderného paktu, přičemž obě partaje se snaží posoudit vyhlídky na úspěch po posledních výměnách v jednáních.",
+        "answer": """Critical:
 no-error
 Major:
 accuracy/addition - "ve Vídni"
@@ -213,21 +237,22 @@ accuracy/omission - "the stop-start"
 Minor:
 terminology/inappropriate for context - "partaje"
 """,
-        },
+    },
     "zhen": {
-            "source_lang": "Chinese",
-            "source_seg": "大众点评乌鲁木齐家居卖场频道为您提供高铁居然之家地址，电话，营业时间等最新商户信息，找装修公司，就上大众点评",
-            "target_lang": "English",
-            "target_seg": "Urumqi Home Furnishing Store Channel provides you with the latest business information such as the address, telephone number, business hours, etc., of high-speed rail, and find a decoration company, and go to the reviews.",
-            "answer": """Critical:
+        "source_lang": "Chinese",
+        "source_seg": "大众点评乌鲁木齐家居卖场频道为您提供高铁居然之家地址，电话，营业时间等最新商户信息，找装修公司，就上大众点评",
+        "target_lang": "English",
+        "target_seg": "Urumqi Home Furnishing Store Channel provides you with the latest business information such as the address, telephone number, business hours, etc., of high-speed rail, and find a decoration company, and go to the reviews.",
+        "answer": """Critical:
 accuracy/addition - "of high-speed rail"
 Major:
 accuracy/mistranslation - "go to the reviews"
 Minor:
 style/awkward - "etc.,"
 """,
-        },
+    },
 }
 
-TEMPLATE_GEMBA_MQM = mqm_fewshot([few_shots['ende'], few_shots['encs'], few_shots['zhen']])
-
+TEMPLATE_GEMBA_MQM = mqm_fewshot(
+    [few_shots["ende"], few_shots["encs"], few_shots["zhen"]]
+)
