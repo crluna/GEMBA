3437e7aa7247802ec6660940d7bb1a25e2a4516c
carlos <carlos.luna@crugroup.com>
2024-08-12 13:16:14 +0100

    Adding formatters and options flag for expanations. Functionlaity is not added yet.

--- evaluator.py
+++ evaluator.py
@@ -2,35 +2,48 @@ import os
 from gemba.utils import get_gemba_scores
 
 
-def compute_mqm_scores(source,
-                       hypothesis,
-                       source_lang='English',
-                       target_lang='Spanish',
-                       method='GEMBA-MQM',
-                       model='gpt-4o'):
-
+def compute_mqm_scores(
+    source,
+    hypothesis,
+    source_lang="English",
+    target_lang="Spanish",
+    method="GEMBA-MQM",
+    model="gpt-4o",
+    expnanations=False,
+):
     if not os.path.isfile(source):
         raise Exception(f"Source file {source} does not exist.")
 
     if not os.path.isfile(hypothesis):
         raise Exception(f"Source file {hypothesis} does not exist.")
 
-    with open(source, 'r') as f:
+    with open(source, "r") as f:
         source = f.readlines()
     source = [x.strip() for x in source]
 
-    with open(hypothesis, 'r') as f:
+    with open(hypothesis, "r") as f:
         hypothesis = f.readlines()
 
     hypothesis = [x.strip() for x in hypothesis]
 
-    assert len(source) == len(hypothesis), "Source and hypothesis files must have the same number of lines."
+    assert len(source) == len(
+        hypothesis
+    ), "Source and hypothesis files must have the same number of lines."
 
-    answers = get_gemba_scores(source, hypothesis, source_lang, target_lang, method, model)
+    if not expnanations:
+        answers = get_gemba_scores(
+            source, hypothesis, source_lang, target_lang, method, model
+        )
+    else:
+        answers = get_gemba_scores(
+            source, hypothesis, source_lang, target_lang, method, model
+        )
 
     return answers
 
+
 if __name__ == "__main__":
     import sys
+
     res = compute_mqm_scores(sys.argv[1], sys.argv[2])
     print(res)
