cccd6db9b46d2edc5cb59be1a2301638ce4ff458
carlos <carlos.luna@crugroup.com>
2024-08-15 07:25:22 +0100

    Remove ipdb as dependency and lints.

--- gemba/gpt_api.py
+++ gemba/gpt_api.py
@@ -1,10 +1,8 @@
 import os
 import sys
 import time
-import ipdb
 import logging
 from termcolor import colored
-from datetime import datetime
 import openai
 import tqdm
 import httpx
@@ -16,29 +14,44 @@ class GptApi:
         self.verbose = verbose
 
         if "OPENAI_AZURE_ENDPOINT" in os.environ:
-            assert "OPENAI_AZURE_KEY" in os.environ, "OPENAI_AZURE_KEY not found in environment"
+            assert (
+                "OPENAI_AZURE_KEY" in os.environ
+            ), "OPENAI_AZURE_KEY not found in environment"
 
             # Azure API access
             self.client = openai.AzureOpenAI(
                 api_key=os.environ["OPENAI_AZURE_KEY"],
                 azure_endpoint=os.environ["OPENAI_AZURE_ENDPOINT"],
-                api_version="2023-07-01-preview"
+                api_version="2023-07-01-preview",
             )
         elif "OPENAI_API_KEY" in os.environ:
             http_client = httpx.Client(verify=False)
             # OpenAI API access
             self.client = openai.OpenAI(
-                    http_client=http_client,
+                http_client=http_client,
                 api_key=os.environ["OPENAI_API_KEY"],
             )
 
         else:
-            raise Exception("OPENAI_API_KEY or OPENAI_AZURE_KEY not found in environment")
+            raise Exception(
+                "OPENAI_API_KEY or OPENAI_AZURE_KEY not found in environment"
+            )
 
-        logging.getLogger().setLevel(logging.CRITICAL)  # in order to suppress all these HTTP INFO log messages
+        logging.getLogger().setLevel(
+            logging.CRITICAL
+        )  # in order to suppress all these HTTP INFO log messages
 
     # answer_id is used for determining if it was the top answer or how deep in the list it was
-    def request(self, prompt, model, parse_response, temperature=0, answer_id=-1, cache=None, max_tokens=None):
+    def request(
+        self,
+        prompt,
+        model,
+        parse_response,
+        temperature=0,
+        answer_id=-1,
+        cache=None,
+        max_tokens=None,
+    ):
         request = {"model": model, "temperature": temperature, "prompt": prompt}
 
         if request in cache and cache[request] is not None and len(cache[request]) > 0:
@@ -49,14 +62,16 @@ class GptApi:
 
         # there is no valid answer
         if len(answers) == 0:
-            return [{
+            return [
+                {
                     "temperature": temperature,
                     "answer_id": answer_id,
                     "answer": None,
                     "prompt": prompt,
                     "finish_reason": None,
                     "model": model,
-                    }]
+                }
+            ]
 
         parsed_answers = []
         for full_answer in answers:
@@ -65,7 +80,14 @@ class GptApi:
             answer_id += 1
             answer = parse_response(full_answer)
             if self.verbose or temperature > 0:
-                print(f"Answer (t={temperature}): " + colored(answer, "yellow") + " (" + colored(full_answer, "blue") + ")", file=sys.stderr)
+                print(
+                    f"Answer (t={temperature}): "
+                    + colored(answer, "yellow")
+                    + " ("
+                    + colored(full_answer, "blue")
+                    + ")",
+                    file=sys.stderr,
+                )
             if answer is None:
                 continue
             parsed_answers.append(
@@ -81,7 +103,14 @@ class GptApi:
 
         # there was no valid answer, increase temperature and try again
         if len(parsed_answers) == 0:
-            return self.request(prompt, model, parse_response, temperature=temperature + 1, answer_id=answer_id, cache=cache)
+            return self.request(
+                prompt,
+                model,
+                parse_response,
+                temperature=temperature + 1,
+                answer_id=answer_id,
+                cache=cache,
+            )
 
         return parsed_answers
 
@@ -95,11 +124,11 @@ class GptApi:
                 break
             except Exception as e:
                 # response was filtered
-                if hasattr(e, 'code'):
-                    if e.code == 'content_filter':
+                if hasattr(e, "code"):
+                    if e.code == "content_filter":
                         return []
                     print(e.code, file=sys.stderr)
-                if hasattr(e, 'error') and e.error['code'] == 'invalid_model_output':
+                if hasattr(e, "error") and e.error["code"] == "invalid_model_output":
                     return []
 
                 # frequent error is reaching the API limit
@@ -115,20 +144,28 @@ class GptApi:
                 answer = choice.message.content.strip()
             else:
                 answer = choice.text.strip()
-                
+
             # one of the responses didn't finish, we need to request more tokens
             if choice.finish_reason != "stop":
                 if self.verbose:
-                    print(colored(f"Increasing max tokens to fit answers.", "red") + colored(answer, "blue"), file=sys.stderr)
+                    print(
+                        colored("Increasing max tokens to fit answers.", "red")
+                        + colored(answer, "blue"),
+                        file=sys.stderr,
+                    )
                 print(f"Finish reason: {choice.finish_reason}", file=sys.stderr)
                 if max_tokens is None:
                     return []
-                return self.request_api(prompt, model, temperature=temperature, max_tokens=max_tokens + 200)
+                return self.request_api(
+                    prompt, model, temperature=temperature, max_tokens=max_tokens + 200
+                )
 
-            answers.append({
-                "answer": answer,
-                "finish_reason": choice.finish_reason,
-            })
+            answers.append(
+                {
+                    "answer": answer,
+                    "finish_reason": choice.finish_reason,
+                }
+            )
 
         if len(answers) > 1:
             # remove duplicate answers
@@ -138,13 +175,13 @@ class GptApi:
 
     def call_api(self, prompt, model, temperature, max_tokens):
         parameters = {
-            "temperature": temperature/10,
+            "temperature": temperature / 10,
             "top_p": 1,
             "n": 1,
             "frequency_penalty": 0,
             "presence_penalty": 0,
             "stop": None,
-            "model": model
+            "model": model,
         }
 
         if max_tokens is not None:
@@ -152,22 +189,30 @@ class GptApi:
 
         if isinstance(prompt, list):
             # check that prompt contain list of dictionaries with role and content
-            assert all(isinstance(p, dict) for p in prompt), "Prompts must be a list of dictionaries."
-            assert all("role" in p and "content" in p for p in prompt), "Prompts must be a list of dictionaries with role and content."
+            assert all(
+                isinstance(p, dict) for p in prompt
+            ), "Prompts must be a list of dictionaries."
+            assert all(
+                "role" in p and "content" in p for p in prompt
+            ), "Prompts must be a list of dictionaries with role and content."
 
             parameters["messages"] = prompt
         else:
-            parameters["messages"] = [{
-                "role": "user",
-                "content": prompt,
-            }]
+            parameters["messages"] = [
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ]
 
         return self.client.chat.completions.create(**parameters)
-    
+
     def bulk_request(self, df, model, parse_mqm_answer, cache, max_tokens=None):
         answers = []
         for i, row in tqdm.tqdm(df.iterrows(), total=len(df), file=sys.stderr):
             prompt = row["prompt"]
-            parsed_answers = self.request(prompt, model, parse_mqm_answer, cache=cache, max_tokens=max_tokens)
+            parsed_answers = self.request(
+                prompt, model, parse_mqm_answer, cache=cache, max_tokens=max_tokens
+            )
             answers += parsed_answers
         return answers
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
 import sys
-import ipdb
-import pandas as pd
-import diskcache as dc
 from absl import app, flags
 from gemba.utils import get_gemba_scores
 
 
-flags.DEFINE_string('method', "GEMBA-MQM", 'Which method to use?')
-flags.DEFINE_string('model', "gpt-4", 'OpenAI model')
-flags.DEFINE_string('source', None, 'Filepath to the source file.')
-flags.DEFINE_string('hypothesis', None, 'Filepath to the translation file.')
-flags.DEFINE_string('source_lang', None, 'Source language name.')
-flags.DEFINE_string('target_lang', None, 'Target language name.')
+flags.DEFINE_string("method", "GEMBA-MQM", "Which method to use?")
+flags.DEFINE_string("model", "gpt-4", "OpenAI model")
+flags.DEFINE_string("source", None, "Filepath to the source file.")
+flags.DEFINE_string("hypothesis", None, "Filepath to the translation file.")
+flags.DEFINE_string("source_lang", None, "Source language name.")
+flags.DEFINE_string("target_lang", None, "Target language name.")
 
 
 def main(argv):
@@ -32,19 +29,29 @@ def main(argv):
     assert FLAGS.target_lang is not None, "Target language name must be provided."
 
     # load both files and strip them
-    with open(FLAGS.source, 'r') as f:
+    with open(FLAGS.source, "r") as f:
         source = f.readlines()
     source = [x.strip() for x in source]
-    with open(FLAGS.hypothesis, 'r') as f:
+    with open(FLAGS.hypothesis, "r") as f:
         hypothesis = f.readlines()
     hypothesis = [x.strip() for x in hypothesis]
 
-    assert len(source) == len(hypothesis), "Source and hypothesis files must have the same number of lines."
+    assert len(source) == len(
+        hypothesis
+    ), "Source and hypothesis files must have the same number of lines."
 
-    answers = get_gemba_scores(source, hypothesis, FLAGS.source_lang, FLAGS.target_lang, FLAGS.method, FLAGS.model)
+    answers = get_gemba_scores(
+        source,
+        hypothesis,
+        FLAGS.source_lang,
+        FLAGS.target_lang,
+        FLAGS.method,
+        FLAGS.model,
+    )
 
     for answer in answers:
         print(answer)
+
 
 if __name__ == "__main__":
     app.run(main)
